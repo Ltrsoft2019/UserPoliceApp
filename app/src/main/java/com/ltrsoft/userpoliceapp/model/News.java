64df105cfec2bb31459915c87f884dda67d2b57c
omkarkshirsaga <okshirsagar700@gmail.com>
2024-02-22 12:53:26 +0530

    committed by omkar

---
+++ app/src/main/java/com/ltrsoft/userpoliceapp/model/News.java
@@ -0,0 +1,30 @@
+package com.ltrsoft.userpoliceapp.model;
+
+public class News {
+
+    private String news_id,	name	,description;
+
+    public String getNews_id() {
+        return news_id;
+    }
+
+    public void setNews_id(String news_id) {
+        this.news_id = news_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
